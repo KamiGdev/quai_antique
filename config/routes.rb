ebeedf4de4cf4f070554c36feec7854d2a8772a3
Kami Gourbail <kami.gourbail@gmail.com>
2023-03-13 15:18:26 +0100

    feat: contact, about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  get '/contact', to: "static_pages#contact"
+  get '/about', to: "static_pages#about"
   root "static_pages#home"
 end
