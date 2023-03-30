eef92ecc025a9a9aa4edefac1201dc20f0a6108a
Kami Gourbail <kami.gourbail@gmail.com>
2023-03-30 18:13:01 +0200

    category

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,10 @@
 class MenusController < ApplicationController
   def index
+    if params[:category]
+      @dishes = Dish.where category: params[:category]
+    else
+      @dishes = Dish.all
+    end
     @menus = Menu.all
   end
 
