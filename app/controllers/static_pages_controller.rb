8811dca632c6a705fb7030020cf2a84dc874ff75
Kami Gourbail <kami.gourbail@gmail.com>
2023-04-24 18:26:46 +0200

    perf(footer): css responsive

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!, except: [:home]
-
   def home
     @bestoff_items = Dish.where(bestoff: true) + Menu.where(bestoff: true)
   end
 
-  def about
-  end
+
 end
